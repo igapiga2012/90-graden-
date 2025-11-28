b703252074762c4e2bba1ba1920a4cbaaf4bd4a5
igapiga2012 <igawarzych@gmail.com>
2025-11-28 11:50:06 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,7 @@
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 90)
+basic.pause(800)
+maqueen.motorStop(maqueen.Motors.All)
+basic.forever(function () {
+	
+})
